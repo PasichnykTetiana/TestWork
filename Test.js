1409ce2c758510465df5082ef48e90f876fa1ccf
Tetiana <t.pasichnyk@ukr.net>
2021-05-28 13:08:38 +0300

    complete

---
+++ Test.js
@@ -0,0 +1,89 @@
+let data = {
+  columns: [
+    {
+      code: 'Острів',
+      name: 'Острів',
+      viewState: 0,
+      isFileContent: false,
+      isFileName: false,
+      isPrintable: true,
+      hint: '',
+      dataType: 'Text',
+    },
+    {
+      code: 'Локація',
+      name: 'Локація',
+      viewState: 0,
+      isFileContent: false,
+      isFileName: false,
+      isPrintable: true,
+      hint: '',
+      dataType: 'Text',
+    },
+    {
+      code: 'Текст',
+      name: 'Текст',
+      viewState: 0,
+      isFileContent: false,
+      isFileName: false,
+      isPrintable: true,
+      hint: '',
+      dataType: 'Text',
+    },
+    {
+      code: 'Дата',
+      name: 'Дата',
+      viewState: 0,
+      isFileContent: false,
+      isFileName: false,
+      isPrintable: true,
+      hint: '',
+      dataType: 'DateTime',
+    },
+    {
+      code: 'Коментар',
+      name: 'Коментар',
+      viewState: 0,
+      isFileContent: false,
+      isFileName: false,
+      isPrintable: true,
+      hint: '',
+      dataType: 'Text',
+    },
+  ],
+  rows: [
+    {
+      values: [
+        'Труханов',
+        'Київ',
+        " столиця та найбільше місто України, одне з найбільших і найстаріших міст Європи. Розташований у середній течії Дніпра, у північній Наддніпрянщині. Політичний, соціально-економічний, транспортний та освітньо-науковий центр країни. Окрема адміністративно-територіальна одиниця в складі України й адміністративний центр Київської області. Адміністративно до складу Київської області не входить. Місце розташування центральних органів влади України, іноземних місій, штаб-квартир більшості підприємств і громадських об'єднань, що працюють в Україні.",
+        '2020-12-12T00:00:00',
+        'Тестове',
+      ],
+    },
+  ],
+};
+
+flexСontainer = document.getElementById('flex-container');
+
+data.columns.map((item) => {
+  return (flexСontainer.innerHTML +=
+    '<div class="block">' + '<div class="title">' + item.name + '</div>' + '</div>');
+});
+
+block = document.getElementsByClassName('block');
+
+let todaysDate = new Date();
+
+let today =
+  ('0' + new Date().getDate()).slice(-2) +
+  '.' +
+  ('0' + (new Date().getMonth() + 1)).slice(-2) +
+  '.' +
+  new Date().getFullYear();
+
+data.rows[0].values.map((item, index) => {
+  return data.columns[index].dataType !== 'DateTime'
+    ? (block[index].innerHTML += '<div class="content">' + item + '</div>')
+    : (block[index].innerHTML += '<div class="content">' + today + '</div>');
+});
